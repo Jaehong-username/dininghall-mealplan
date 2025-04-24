7c9f07f07df33f8d0d732d21568546b867973574
Hannah Garcia <hkgarcia04@gmail.com>
2025-04-24 13:40:51 -0700

    updating code after merge, ensuring current layout is up-to-date

--- src/app.py
+++ src/app.py
@@ -18,11 +18,10 @@ import os
 from werkzeug.utils import secure_filename
 
 
-
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "data/database.db")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/14253/Desktop/dininghall-mealplan-1/data/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "data/database.db")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/14253/Desktop/dininghall-mealplan-1/data/database.db'
 bcrypt = Bcrypt(app)
 
 
@@ -42,12 +41,12 @@ app.register_blueprint(models_bp)
 # Create all databases if they don't exist
 db.init_app(app)
 
+# sample data for database
 def create_db():
     with app.app_context():
         db.create_all()
         
         # TEMP STUDENT FOR TESTING PURPOSES (since no info in database yet)
-
         # to clean out (ONLY DO WITH NON-IMPORTANT DB): rm database.db & rerun code after commenting code out
         existing_user = User.query.filter_by(user_id=1).first()
 
@@ -75,41 +74,6 @@ def create_db():
 
             db.session.add(temp_student)
             db.session.commit()
-
-
-
-### -- Routes -- ###
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-# choose meal plan (with id)
-@app.route("/<int:student_id>/meal-plan", methods=["GET", "POST"])
-def meal_plan_id(student_id):
-
-    # find student in database
-    student = Student.query.filter_by(user_id=student_id).first()
-    
-    # display error & return to home if student not found
-    if not student:
-        print("Student does not exist")
-        return "<h3>Student does not exist!</h3>" \
-        "<form action='/'><button type='submit'>Return Home</button></form>"
-    
-    # updating meal plan
-    if request.method == 'POST':
-        print("Now changing meal plan")
-        updated_plan = request.form.get("plan_id")
-        student.plan_id = int(updated_plan)
-        db.session.commit() # update in database
-
-    return render_template("meal-plan.html", student=student)
-
-# choose meal plan (without id)
-@app.route("/meal-plan")
-def meal_plan():
-    return render_template("meal-plan.html")
-
 
         # BASE TYPES FOR TESTING
         base_types = ["Breakfast", "Lunch", "Brunch", "Dinner"]
@@ -275,8 +239,39 @@ def meal_plan():
 
             db.session.add(temp_menu)
             db.session.commit()
-        
 
+# ### -- Routes -- ###
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+# # choose meal plan (with id)
+# @app.route("/<int:student_id>/meal-plan", methods=["GET", "POST"])
+# def meal_plan_id(student_id):
+
+#     # find student in database
+#     student = Student.query.filter_by(user_id=student_id).first()
+    
+#     # display error & return to home if student not found
+#     if not student:
+#         print("Student does not exist")
+#         return "<h3>Student does not exist!</h3>" \
+#         "<form action='/'><button type='submit'>Return Home</button></form>"
+    
+#     # updating meal plan
+#     if request.method == 'POST':
+#         print("Now changing meal plan")
+#         updated_plan = request.form.get("plan_id")
+#         student.plan_id = int(updated_plan)
+#         db.session.commit() # update in database
+
+#     return render_template("meal-plan.html", student=student)
+
+# # choose meal plan (without id)
+# @app.route("/meal-plan")
+# def meal_plan():
+#     return render_template("meal-plan.html")
+        
 ### Program entrypoint (place at bottom of script)
 create_db() ## Create all databasees if they don't exist
 
